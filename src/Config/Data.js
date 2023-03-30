7ccdae2698416dff858df0437f3182d7be71d878
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-03-29 23:39:56 -0700

    Home Slider Content Update

---
+++ src/Config/Data.js
@@ -0,0 +1,27 @@
+import img1 from '../Images/Home-Slider/item1.jpg'
+import img2 from '../Images/Home-Slider/item2.jpg'
+import img3 from '../Images/Home-Slider/item3.jpg'
+import img4 from '../Images/Home-Slider/item4.avif'
+
+export const sliderData = [
+    {
+        image: img1,
+        catagory: 'Fruits'
+    },
+    {
+        image: img2,
+        catagory: 'Vegetables'
+    },
+    {
+        image: img3,
+        catagory: 'Meat'
+    },
+    {
+        image: img4,
+        catagory: 'Grocery'
+    },
+    {
+        image: img1,
+        catagory: 'Masala'
+    },
+]
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -3,7 +3,36 @@ import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput, FlatList } 
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// import img1 from '../Images/Home-Slider/item1.jpg'
+// import img2 from '../Images/Home-Slider/item2.jpg'
+// import img3 from '../Images/Home-Slider/item3.jpg'
+// import img4 from '../Images/Home-Slider/item4.avif'
+
+const data = [
+    {
+        // image: img1,
+        catagory: 'Fruits'
+    },
+    {
+        // image: img2,
+        catagory: 'Vegetables'
+    },
+    {
+        // image: img3,
+        catagory: 'Meat'
+    },
+    {
+        // image: img4,
+        catagory: 'Grocery'
+    },
+    {
+        // image: img1,
+        catagory: 'Masala'
+    },
+]
+
 const Home = () => {
+
     return (
         // MAIN-CONTAINER.
         <View style={styles.homeContainer}>
@@ -39,21 +68,73 @@ const Home = () => {
             </View>
 
             {/* CATOGERY-SLIDER. */}
-            <View style={styles.catogery}>
-                <Text>Shop by Category</Text>
-
+            <View
+                style={{
+                    backgroundColor: 'lightblue',
+                    marginVertical: 10
+                }}
+            >
+                <Text
+                    style={{
+                        fontWeight: 'bold',
+                        color: 'gray',
+                        fontSize: 15
+                    }}
+                >
+                    Shop by Category
+                </Text>
+                <FlatList
+                    data={data}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <View
+                                style={{
+                                    backgroundColor: 'lightgray',
+                                    paddingHorizontal: 5,
+                                    paddingVertical: 5,
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <Image
+                                    source={require('../Images/Home-Slider/item1.jpg')}
+                                    style={{
+                                        width: 90,
+                                        height: 50,
+                                        borderRadius: 10,
+                                        marginVertical: 5,
+                                        borderColor: 'green',
+                                        borderWidth: 2,
+                                    }}
+                                />
+                                <Text
+                                    style={{
+                                        backgroundColor: 'gray',
+                                        width: 90,
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {item.catagory}
+                                </Text>
+                            </View>
+                        )
+                    }}
+                />
             </View>
 
             {/* LIST-ITEMS. */}
-            <View style={styles.listItems}>
+            {/* <View style={styles.listItems}>
                 <FlatList
                 // data={DATA}
                 // renderItem={({ item }) => <Item title={item.title} />}
                 // keyExtractor={item => item.id}
                 />
-            </View>
+            </View> */}
 
-        </View>
+        </View >
     )
 }
 
@@ -91,7 +172,4 @@ const styles = StyleSheet.create({
         color: '#0c6fa6',
         width: 200,
     },
-
-    // IMAGE-BOX.
-    imageBox: {}
 })
