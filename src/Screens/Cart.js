f936957b8ab820eee7019b35f1c7a8c6aa458f8e
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-03-31 01:55:37 -0700

    Cart screen update

--- src/Screens/Cart.js
+++ src/Screens/Cart.js
@@ -44,17 +44,20 @@ const Cart = () => {
                     <FlatList data={data} renderItem={({ item, index }) => {
                         return (
                             <View style={styles.listItem}>
-                                <Image source={require('../Images/Home-Slider/item1.jpg')} style={styles.listItemImg} />
+                                <View style={styles.listItemImgBox}>
+                                    <Image source={require('../Images/Home-Slider/item1.jpg')} style={styles.listItemImg} />
+                                    <Text style={styles.catagory}>{item.catagory}</Text>
+                                </View>
                                 <View style={styles.listItemQuantityBox}>
                                     <View style={styles.removeQuantity}>
-                                        <Ionicons name='remove' color='black' size={15} />
+                                        <Ionicons name='remove' color='white' size={20} />
                                     </View>
                                     <Text style={styles.quantity}>{item.quantity}</Text>
                                     <View style={styles.addQuantity}>
-                                        <Ionicons name='add' color='black' size={15} />
+                                        <Ionicons name='add' color='white' size={20} />
                                     </View>
                                 </View>
-                                <Text>Rs: {item.price}</Text>
+                                <Text style={styles.price}>Rs: {item.price}</Text>
                             </View>
                         )
                     }} />
@@ -72,19 +75,19 @@ const styles = StyleSheet.create({
     cartBox: {
         flex: 1,
         paddingHorizontal: 10,
-        backgroundColor: "lightblue",
+        // backgroundColor: "lightblue",
     },
 
     // TOP-NAV.
     nav: {
-        flex: 0.1,
+        // flex: 0.1,
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
         borderBottomWidth: 1,
         borderBottomColor: 'lightgray',
-        backgroundColor: '#81c5f5',
+        // backgroundColor: '#81c5f5',
     },
     navText1: {
         fontSize: 20,
@@ -101,24 +104,38 @@ const styles = StyleSheet.create({
 
     // LIST-ITEMS.
     listContainer: {
-        backgroundColor: '#d6ecfb',
-        flex: 0.5,
+        // backgroundColor: '#d6ecfb',
+        // flex: 0.43,
+        paddingBottom: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray',
     },
     listItem: {
         display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        marginTop: 5,
-        backgroundColor: '#0098ff',
+        marginTop: 10,
+        // backgroundColor: '#0098ff',
+    },
+    listItemImgBox: {
+        display: 'flex',
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     listItemImg: {
-        width: 85,
-        height: 60,
-        borderRadius: 10,
-        marginTop: 5,
-        borderColor: '#6cd205',
+        width: 100,
+        height: 70,
         borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#6cd205',
+    },
+    catagory: {
+        width: 80,
+        marginLeft: 5,
+        color: 'black',
+        fontWeight: 'bold',
     },
     listItemQuantityBox: {
         display: 'flex',
@@ -126,13 +143,29 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
     },
-    quantity: {
-        backgroundColor: '#6cd205',
-    },
     removeQuantity: {
-        backgroundColor: 'gray',
+        padding: 2,
+        borderRadius: 5,
+        backgroundColor: 'red',
+    },
+    quantity: {
+        fontWeight: 'bold',
+        marginHorizontal: 5,
+        paddingHorizontal: 5,
+        color: 'black',
+        backgroundColor: 'black',
+        color: 'white',
+        borderRadius: 50,
     },
     addQuantity: {
+        padding: 2,
+        borderRadius: 5,
         backgroundColor: '#6cd205',
+    },
+    price: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        width: 50,
+        color: 'black',
     },
 })
